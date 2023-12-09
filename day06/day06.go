ac44541820f838339231c587f70cdcc5afcb47cf
Stefan Waldmann <Stefan.Waldmann@partner.bmwgroup.com>
2023-12-09 16:08:45 +0100

    solve day 06 - part 2

--- day06/day06.go
+++ day06/day06.go
@@ -6,14 +6,22 @@ import (
 	"swads/aoc2023/aocutils"
 )
 
-var example = []string{
+var input1 = []string{
 	"Time:        47     84     74     67",
 	"Distance:   207   1394   1209   1014",
 }
 
+var input2 = []string{
+	"Time:              47847467",
+	"Distance:   207139412091014",
+}
+
 func Solve() {
-	part1 := SolvePart1(example)
+	part1 := SolvePart1(input1)
 	fmt.Println("Day 06 - part1:", part1)
+
+	part2 := SolvePart1(input2)
+	fmt.Println("Day 06 - part2:", part2)
 }
 
 func SolvePart1(input []string) int {
@@ -36,4 +44,34 @@ func SolvePart1(input []string) int {
 	}
 
 	return result
+}
+
+func SolvePart2(input []string) int {
+	time := strings.Fields(input[0])[1]
+	distance := strings.Fields(input[1])[1]
+	millis := aocutils.StrToInt(time)
+	maxDist := aocutils.StrToInt(distance)
+
+	minWinning := 0
+	maxWinning := millis
+
+	for holdTime := 1; holdTime < millis; holdTime++ {
+		rest := millis - holdTime
+		dist := holdTime * rest
+		if dist > maxDist {
+			minWinning = holdTime
+			println("minWinning:", minWinning)
+			break
+		}
+	}
+	for holdTime := millis; holdTime > 0; holdTime-- {
+		rest := millis - holdTime
+		dist := holdTime * rest
+		if dist > maxDist {
+			maxWinning = holdTime
+			println("maxWinning:", maxWinning)
+			break
+		}
+	}
+	return maxWinning - minWinning + 1
 }
--- day06/day06_test.go
+++ day06/day06_test.go
@@ -6,13 +6,24 @@ import (
 	"github.com/stretchr/testify/assert"
 )
 
-var input = []string{
+var example1 = []string{
 	"Time:      7  15   30",
 	"Distance:  9  40  200",
 }
 
+var example2 = []string{
+	"Time:      71530",
+	"Distance:  940200",
+}
+
 func TestSolvePart1(t *testing.T) {
-	result := SolvePart1(input)
+	result := SolvePart1(example1)
 
 	assert.Equal(t, 288, result)
+}
+
+func TestSolvePart2(t *testing.T) {
+	result := SolvePart2(example2)
+
+	assert.Equal(t, 71503, result)
 }
