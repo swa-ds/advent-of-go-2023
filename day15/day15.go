41d2b89c1ed77d8ea232a446d68540fdb88fe3bf
Stefan Waldmann <stefan.waldmann@doubleslash.de>
2023-12-27 15:13:43 +0100

    solve day 15 - part 2

--- day15/day15.go
+++ day15/day15.go
@@ -3,14 +3,17 @@ package day15
 import (
 	"fmt"
 	"strings"
-	"swads/aoc2023/aocutils"
+	util "swads/aoc2023/aocutils"
 )
 
 func Solve() {
-	input := aocutils.ReadFile("day15/input.txt")
+	input := util.ReadFile("day15/input.txt")
 
 	part1 := SolvePart1(input)
 	fmt.Println("Day 15 - Part 1:", part1)
+
+	part2 := SolvePart2(input)
+	fmt.Println("Day 15 - Part 2:", part2)
 
 }
 
@@ -23,6 +26,59 @@ func SolvePart1(input string) int {
 	return result
 }
 
+func SolvePart2(input string) int {
+	result := 0
+	steps := strings.Split(input, ",")
+	boxes := make([][]Box, 256)
+	for _, step := range steps {
+		var label string
+		if strings.Contains(step, "-") {
+			label = step[:len(step)-1]
+			remove(label, boxes)
+		} else {
+			split := strings.Split(step, "=")
+			label = split[0]
+			focLen := util.StrToInt(split[1])
+			put(Box{label, focLen}, boxes)
+		}
+	}
+
+	for i, lenses := range boxes {
+		if len(lenses) > 0 {
+			//fmt.Println("Box", i, lenses)
+			for j, lens := range lenses {
+				result += (i + 1) * (j + 1) * lens.focalLen
+			}
+		}
+	}
+
+	return result
+}
+
+func put(box Box, boxes [][]Box) {
+	hash := hash(box.label)
+	lenses := boxes[hash]
+	for i, boxInSlot := range lenses {
+		if boxInSlot.label == box.label {
+			lenses[i] = box
+			boxes[hash] = lenses
+			return
+		}
+	}
+	boxes[hash] = append(boxes[hash], box)
+}
+
+func remove(label string, boxes [][]Box) {
+	hash := hash(label)
+	lenses := boxes[hash]
+	for i, box := range lenses {
+		if box.label == label {
+			lenses = append(lenses[:i], lenses[i+1:]...)
+			boxes[hash] = lenses
+		}
+	}
+}
+
 func hash(s string) int {
 	hash := 0
 	for _, r := range []rune(s) {
@@ -31,4 +87,9 @@ func hash(s string) int {
 		hash = hash % 256
 	}
 	return hash
+}
+
+type Box struct {
+	label    string
+	focalLen int
 }
--- day15/day15_test.go
+++ day15/day15_test.go
@@ -1,16 +1,24 @@
 package day15
 
 import (
+	"fmt"
 	"testing"
 
 	"github.com/stretchr/testify/assert"
 )
 
+var input = "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
+
 func TestSolvePart1(t *testing.T) {
-	input := "rn=1,cm-,qp=3,cm=2,qp-,pc=4,ot=9,ab=5,pc-,pc=6,ot=7"
 	part1 := SolvePart1(input)
 
 	assert.Equal(t, 1320, part1)
+}
+
+func TestSolvePart2(t *testing.T) {
+	part2 := SolvePart2(input)
+
+	assert.Equal(t, 145, part2)
 }
 
 func TestHash(t *testing.T) {
@@ -19,4 +27,11 @@ func TestHash(t *testing.T) {
 
 	h = hash("rn=1")
 	assert.Equal(t, 30, h)
+}
+
+func TestXyz(t *testing.T) {
+	fmt.Println(hash("rn"))
+	fmt.Println(hash("qp"))
+	fmt.Println(hash("pc"))
+	fmt.Println(hash("ot"))
 }
