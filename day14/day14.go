7a05d01b0b716fb4cfe11404a4544dd05c210809
Stefan Waldmann <stefan.waldmann@doubleslash.de>
2023-12-27 14:13:03 +0100

    solve day 14 - part 2

--- day14/day14.go
+++ day14/day14.go
@@ -2,6 +2,7 @@ package day14
 
 import (
 	"fmt"
+	"reflect"
 	util "swads/aoc2023/aocutils"
 )
 
@@ -11,18 +12,69 @@ func Solve() {
 	part1 := SolvePart1(input)
 	fmt.Println("Day 14 - Part 1:", part1)
 
-	//part2 := SolvePart2(input)
-	//fmt.Println("Day 14 - Part 2:", part2)
+	part2 := SolvePart2(input)
+	fmt.Println("Day 14 - Part 2:", part2)
 }
 
 func SolvePart1(input []string) int {
 	platform := util.LinesToMatrix(input)
-	tiltNorth(platform)
-	weight := len(platform)
+	platform = tiltNorth(platform)
+
+	return weightOnNorthBeam(platform)
+}
+
+func SolvePart2(input []string) int {
+	encountered := []Encountered{}
+	platform := util.LinesToMatrix(input)
+	var loopLen int
+	var initial int
+
+	for i := 1; true; i++ {
+		//fmt.Println("Cycle", i)
+		platform = spinCycle(platform)
+		//printMatrix(platform)
+		idxEnc := indexOf(encountered, platform)
+		if idxEnc >= 0 {
+			//fmt.Printf("Detected loop after %d spin cycles, "+
+			//	"pattern first encountered at position %d\n", i, idxEnc)
+			loopLen = len(encountered) - idxEnc
+			initial = i - loopLen
+			//fmt.Println("Loop length:", loopLen, "initial:", initial)
+			break
+		}
+		encountered = append(encountered, Encountered{platform})
+	}
+
+	//fmt.Println("saved patterns: ", len(encountered))
+
+	loops := (1_000_000_000 - initial) / loopLen
+	rest := 1_000_000_000 - loopLen*loops - initial
+
+	//fmt.Println("initial:", initial, "loops:", loops, "rest:", rest)
+	//for i, enc := range encountered {
+	//	fmt.Println("i:", i, "weight:", weightOnNorthBeam(enc.pattern))
+	//}
+
+	platform = encountered[initial+rest-1].pattern
+
+	return weightOnNorthBeam(platform)
+}
+
+func indexOf(encountered []Encountered, platform [][]rune) int {
+	for i, enc := range encountered {
+		if equal(platform, enc.pattern) {
+			return i
+		}
+	}
+	return -1
+}
+
+func weightOnNorthBeam(original [][]rune) int {
 	result := 0
-	for row := 0; row < len(platform); row++ {
-		for col := 0; col < len(platform[row]); col++ {
-			if (platform[row][col]) == 'O' {
+	weight := len(original)
+	for row := 0; row < len(original); row++ {
+		for col := 0; col < len(original[row]); col++ {
+			if (original[row][col]) == 'O' {
 				result += weight
 			}
 		}
@@ -31,18 +83,82 @@ func SolvePart1(input []string) int {
 	return result
 }
 
-func tiltNorth(platform [][]rune) {
-	for col := 0; col < len(platform[0]); col++ {
+func spinCycle(original [][]rune) [][]rune {
+	result := original
+	for i := 0; i < 4; i++ {
+		result = tiltNorth(result)
+		result = rotateMatrix(result)
+	}
+
+	return result
+}
+
+func tiltNorth(original [][]rune) [][]rune {
+	rows := len(original)
+	cols := len(original[0])
+
+	tilted := clone(original)
+	for col := 0; col < cols; col++ {
 		moved := true
 		for i := 0; moved; i++ {
 			moved = false
-			for row := 0; row < len(platform)-1; row++ {
-				if platform[row][col] == '.' && platform[row+1][col] == 'O' {
-					platform[row][col] = 'O'
-					platform[row+1][col] = '.'
+			for row := 0; row < rows-1; row++ {
+				if tilted[row][col] == '.' && tilted[row+1][col] == 'O' {
+					tilted[row][col] = 'O'
+					tilted[row+1][col] = '.'
 					moved = true
 				}
 			}
 		}
 	}
+	return tilted
+}
+
+func clone(original [][]rune) [][]rune {
+	rows := len(original)
+	cols := len(original[0])
+
+	copy := make([][]rune, rows)
+	for i := 0; i < len(copy); i++ {
+		copy[i] = make([]rune, cols)
+		for j := 0; j < len(copy[i]); j++ {
+			copy[i][j] = original[i][j]
+		}
+	}
+	return copy
+}
+
+// function kindly provided by ChatGPT ;-)
+func rotateMatrix(original [][]rune) [][]rune {
+	rows := len(original)
+	cols := len(original[0])
+	// Create a new matrix with swapped dimensions
+	rotated := make([][]rune, cols)
+	for i := range rotated {
+		rotated[i] = make([]rune, rows)
+	}
+
+	// Transpose the original matrix and reverse the order of rows
+	for row := 0; row < rows; row++ {
+		for col := 0; col < cols; col++ {
+			rotated[col][rows-row-1] = original[row][col]
+		}
+	}
+
+	return rotated
+}
+
+func printMatrix(afterCycle [][]rune) {
+	for _, row := range afterCycle {
+		fmt.Println(string(row))
+	}
+	fmt.Println("")
+}
+
+func equal(a [][]rune, b [][]rune) bool {
+	return reflect.DeepEqual(a, b)
+}
+
+type Encountered struct {
+	pattern [][]rune
 }
--- day14/day14_test.go
+++ day14/day14_test.go
@@ -34,17 +34,129 @@ var expTilted = []string{
 	"#....#....",
 }
 
+var expOneCycle = []string{
+	".....#....",
+	"....#...O#",
+	"...OO##...",
+	".OO#......",
+	".....OOO#.",
+	".O#...O#.#",
+	"....O#....",
+	"......OOOO",
+	"#...O###..",
+	"#..OO#....",
+}
+
+var expThreeCycles = []string{
+	".....#....",
+	"....#...O#",
+	".....##...",
+	"..O#......",
+	".....OOO#.",
+	".O#...O#.#",
+	"....O#...O",
+	".......OOO",
+	"#...O###.O",
+	"#.OOO#...O",
+}
+
 func TestSolvePart1(t *testing.T) {
 	result := SolvePart1(input)
 
 	assert.Equal(t, 136, result)
 }
 
+func TestSolvePart2(t *testing.T) {
+	result := SolvePart2(input)
+
+	assert.Equal(t, 64, result)
+}
+
+func TestSpinCycle(t *testing.T) {
+	platform := util.LinesToMatrix(input)
+	result := spinCycle(platform)
+	exp := util.LinesToMatrix(expOneCycle)
+
+	assert.Equal(t, exp, result)
+
+	result = spinCycle(result)
+	result = spinCycle(result)
+	exp = util.LinesToMatrix(expThreeCycles)
+
+	assert.Equal(t, exp, result)
+}
+
 func TestTiltNorth(t *testing.T) {
 	platform := util.LinesToMatrix(input)
 	exp := util.LinesToMatrix(expTilted)
-	tiltNorth(platform)
+	tilted := tiltNorth(platform)
 
-	assert.Equal(t, exp, platform)
+	assert.Equal(t, exp, tilted)
+	assert.NotEqual(t, exp, platform)
+}
 
+func TestRotateMatrix(t *testing.T) {
+	original := [][]rune{
+		{'1', '2', '3', '4'},
+		{'5', '6', '7', '8'},
+	}
+	exp := [][]rune{
+		{'5', '1'},
+		{'6', '2'},
+		{'7', '3'},
+		{'8', '4'},
+	}
+
+	// rotate 1st time
+	rotated := rotateMatrix(original)
+
+	assert.Equal(t, exp, rotated)
+
+	// rotate 2nd time
+	exp = [][]rune{
+		{'8', '7', '6', '5'},
+		{'4', '3', '2', '1'},
+	}
+	rotated = rotateMatrix(rotated)
+
+	assert.Equal(t, exp, rotated)
+
+	// rotate 3rd time
+	exp = [][]rune{
+		{'4', '8'},
+		{'3', '7'},
+		{'2', '6'},
+		{'1', '5'},
+	}
+	rotated = rotateMatrix(rotated)
+
+	assert.Equal(t, exp, rotated)
+
+	// rotate 4th time
+	rotated = rotateMatrix(rotated)
+
+	assert.Equal(t, original, rotated)
+}
+
+func TestEqual(t *testing.T) {
+	a := util.LinesToMatrix(input)
+	b := util.LinesToMatrix(input)
+
+	assert.True(t, equal(a, b))
+
+	b = util.LinesToMatrix(expTilted)
+
+	assert.False(t, equal(a, b))
+}
+
+func TestIndexOf(t *testing.T) {
+	encountered := []Encountered{}
+	encountered = append(encountered, Encountered{util.LinesToMatrix(input)})
+	encountered = append(encountered, Encountered{util.LinesToMatrix(expThreeCycles)})
+
+	exp3 := util.LinesToMatrix(expThreeCycles)
+	assert.Equal(t, 1, indexOf(encountered, exp3))
+
+	tilted := util.LinesToMatrix(expTilted)
+	assert.Equal(t, -1, indexOf(encountered, tilted))
 }
