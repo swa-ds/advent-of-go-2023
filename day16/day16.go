dad2a742cb23b64f353c96e2965899a076c608ac
Stefan Waldmann <stefan.waldmann@doubleslash.de>
2023-12-28 15:44:41 +0100

    solve day 16 - part 2

--- day16/day16.go
+++ day16/day16.go
@@ -2,6 +2,7 @@ package day16
 
 import (
 	"fmt"
+	"slices"
 	util "swads/aoc2023/aocutils"
 
 	mapset "github.com/deckarep/golang-set/v2"
@@ -28,14 +29,14 @@ const (
 	Right
 )
 
-// save seen beams to prevent infinite loops
-var seen = mapset.NewSet[Beam]()
-
 func Solve() {
 	input := util.ReadLines("day16/input.txt")
 
 	part1 := SolvePart1(input)
 	fmt.Println("Day 16 - Part 1:", part1)
+
+	part2 := SolvePart2(input)
+	fmt.Println("Day 16 - Part 2:", part2)
 }
 
 func SolvePart1(input []string) int {
@@ -43,20 +44,48 @@ func SolvePart1(input []string) int {
 
 	beam := NewBeam(Right, Position{0, 0})
 
-	energized := mapset.NewSet[Position]()
+	return energize(beam, grid)
+}
 
-	run(beam, grid, energized)
+func SolvePart2(input []string) int {
+	grid := util.LinesToMatrix(input)
+	beams := []Beam{}
+
+	rows := len(grid)
+	cols := len(grid[0])
+
+	for row := 0; row < rows; row++ {
+		beams = append(beams, Beam{Right, Position{0, row}})
+		beams = append(beams, Beam{Left, Position{cols - 1, row}})
+	}
+	for col := 0; col < cols; col++ {
+		beams = append(beams, Beam{Down, Position{col, 0}})
+		beams = append(beams, Beam{Up, Position{col, rows - 1}})
+	}
+	//fmt.Println(beams)
+
+	energized := []int{}
+
+	for _, beam := range beams {
+		energized = append(energized, energize(beam, grid))
+	}
+
+	return slices.Max(energized)
+}
+
+func energize(beam Beam, grid [][]rune) int {
+	energized := mapset.NewSet[Position]()
+	// save seen beams to prevent infinite loops
+	seen := mapset.NewSet[Beam]()
+
+	run(beam, grid, energized, seen)
 
 	//printEnergized(grid, energized)
 
 	return energized.Cardinality()
 }
 
-func SolvePart2(input []string) int {
-	return 0
-}
-
-func run(beam Beam, grid [][]rune, energized mapset.Set[Position]) {
+func run(beam Beam, grid [][]rune, energized mapset.Set[Position], seen mapset.Set[Beam]) {
 	seen.Add(beam)
 	//fmt.Println("starting new run", beam)
 	//defer fmt.Println("ending run", beam)
@@ -75,7 +104,7 @@ func run(beam Beam, grid [][]rune, energized mapset.Set[Position]) {
 			} else if grid[beam.pos.y][beam.pos.x] == '|' {
 				beam.direction = Down
 				newBeam := NewBeam(Up, Position{beam.pos.x, beam.pos.y - 1})
-				run(newBeam, grid, energized)
+				run(newBeam, grid, energized, seen)
 			}
 		} else if beam.direction == Left {
 			if grid[beam.pos.y][beam.pos.x] == '/' {
@@ -85,7 +114,7 @@ func run(beam Beam, grid [][]rune, energized mapset.Set[Position]) {
 			} else if grid[beam.pos.y][beam.pos.x] == '|' {
 				beam.direction = Down
 				newBeam := NewBeam(Up, Position{beam.pos.x, beam.pos.y - 1})
-				run(newBeam, grid, energized)
+				run(newBeam, grid, energized, seen)
 			}
 		} else if beam.direction == Up {
 			if grid[beam.pos.y][beam.pos.x] == '/' {
@@ -95,7 +124,7 @@ func run(beam Beam, grid [][]rune, energized mapset.Set[Position]) {
 			} else if grid[beam.pos.y][beam.pos.x] == '-' {
 				beam.direction = Left
 				newBeam := NewBeam(Right, Position{beam.pos.x + 1, beam.pos.y})
-				run(newBeam, grid, energized)
+				run(newBeam, grid, energized, seen)
 			}
 		} else if beam.direction == Down {
 			if grid[beam.pos.y][beam.pos.x] == '/' {
@@ -105,7 +134,7 @@ func run(beam Beam, grid [][]rune, energized mapset.Set[Position]) {
 			} else if grid[beam.pos.y][beam.pos.x] == '-' {
 				beam.direction = Left
 				newBeam := NewBeam(Right, Position{beam.pos.x + 1, beam.pos.y})
-				run(newBeam, grid, energized)
+				run(newBeam, grid, energized, seen)
 			}
 		}
 		if beam.direction == Up {
--- day16/day16_test.go
+++ day16/day16_test.go
@@ -1,6 +1,7 @@
 package day16
 
 import (
+	"slices"
 	"strings"
 	"testing"
 
@@ -26,6 +27,12 @@ func TestSolvePart1(t *testing.T) {
 	assert.Equal(t, 46, part1)
 }
 
+func TestSolvePart2(t *testing.T) {
+	part1 := SolvePart2(input)
+
+	assert.Equal(t, 51, part1)
+}
+
 func TestMoveRight(t *testing.T) {
 	beam := NewBeam(Right, Position{1, 1})
 	exp := Position{2, 1}
@@ -33,4 +40,10 @@ func TestMoveRight(t *testing.T) {
 	moveRight(&beam)
 
 	assert.Equal(t, exp, beam.pos)
+}
+
+func TestMax(t *testing.T) {
+	ints := []int{2, 9, 17, 42, 15, 11}
+
+	assert.Equal(t, 42, slices.Max(ints))
 }
